885369fb9877741024b11da84d0a8473ad8bffbe
EPAM\Ankit_Soni <ankit_Soni@epam.com>
2023-11-03 08:10:52 +0530

    Versioning Configuration

---
+++ Versioning/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Versioning.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+            });
+        }
+    }
+}
